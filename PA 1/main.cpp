ed401a1620ef70c78ff2f7e1fb37913168159b2b
Anthony Thai <anthonythai229@gmail.com>
2026-08-27 02:11:42 -0500

    Redid Excel Code
    Entirely remade the file code to accommodate for 4 algorithm

--- PA 1/main.cpp
+++ PA 1/main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <random>
 #include <fstream>
+#include <vector>
+
 #include "BF_V1"
 #include "BF_V2"
 
@@ -11,18 +13,65 @@ int main() {
     mt19937 gen(rd());
     uniform_int_distribution<int> dist(1, 1000000);
 
-    ofstream file("BF_V1_Results.csv");
-    file << "Number One,Number Two,Their GCD,Time Spent (Milliseconds)\n";
+    vector<pair<int, int>> numbers;
 
 
     for (int i = 0; i < 1000; i++) {
         int a = dist(gen);
         int b = dist(gen);
 
-        file << a << "," << b << "," << BF_v1(a, b) << "\n";
+        numbers.push_back({a, b});
 
     }
-    file.close();
+
+    // Write results to CSV file for BF_V1
+    ofstream bf1File("BF_v1_Results.csv");
+
+    bf1File << "Number One,Number Two,Their GCD,Time Spent (Milliseconds)\n";
+
+    vector<double> bf1Times;
+
+    for (pair<int, int> p : numbers) {
+
+        auto start = chrono::high_resolution_clock::now();
+
+        int gcd = BF_v1(p.first, p.second);
+
+        auto end = chrono::high_resolution_clock::now();
+
+        chrono::duration<double, std::milli> elapsed = end - start;
+
+        bf1Times.push_back(elapsed.count());
+
+        bf1File << p.first << "," << p.second << "," << gcd << "," << elapsed.count() << "\n";
+    }
+
+    bf1File.close();
+
+    // Write results to CSV file for BF_V2
+    ofstream bf2File("BF_v2Results.csv");
+
+    bf2File << "Number One,Number Two,Their GCD,Time Spent (Milliseconds)\n";
+
+    vector<double> bf2Times;
+
+    for (pair<int, int> p : numbers) {
+
+        auto start = chrono::high_resolution_clock::now();
+
+        int gcd = BF_v2(p.first, p.second);
+
+        auto end = chrono::high_resolution_clock::now();
+
+        chrono::duration<double, std::milli> elapsed = end - start;
+
+        bf2Times.push_back(elapsed.count());
+
+        bf2File << p.first << "," << p.second << "," << gcd << "," << elapsed.count() << "\n";
+    }
+
+    bf2File.close();
+
 
     return 0;
 }
